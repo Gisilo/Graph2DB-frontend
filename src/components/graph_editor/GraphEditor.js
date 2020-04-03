4cbaff79a89d9e0cc0f1d1058cdf958838531e61
Simone-Damico <simodamico1992@gmail.com>
2020-04-03 19:29:40 +0200

    Add mxgraph component

--- src/components/graph_editor/GraphEditor.js
+++ src/components/graph_editor/GraphEditor.js
@@ -2,9 +2,13 @@ import React from 'react';
 import './graph_editor.css'
 import './graph_editor'
 
+const gx = require("mxgraph");
+
 export class GraphEditor extends React.Component {
 
     render() {
+        console.log("sss");
+
         return (
                 <div className="./graph_editor.css">
                     <script src="./graph_editor.js" />
