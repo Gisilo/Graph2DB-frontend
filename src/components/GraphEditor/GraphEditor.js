e5ce977c4ddfb25c054d3a84857092318dd9b111
Simone-Damico <simodamico1992@gmail.com>
2020-04-04 21:48:40 +0200

    Add new node via click on canvas

--- src/components/GraphEditor/GraphEditor.js
+++ src/components/GraphEditor/GraphEditor.js
@@ -1,81 +1,62 @@
-// App.js
-import React from 'react';
+import React, { Component } from 'react'
+import CytoscapeComponent from 'react-cytoscapejs'
 
-import * as go from 'gojs';
-import { ReactDiagram } from 'gojs-react';
+export class GraphEditor extends Component {
 
-import './GraphEditor.css';  // contains .diagram-component CSS
 
-// ...
+    state = {
+        w: 0,
+        h: 0,
+        elements: [
+            { data: { id: 'one', label: 'Node 1wwww' }, position: { x: 200, y: 200 } },
+            { data: { id: 'two', label: 'Node 2' }, position: { x: 100, y: 0 }},
+            { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2'}}
+        ]
+    };
 
-/**
- * Diagram initialization method, which is passed to the ReactDiagram component.
- * This method is responsible for making the diagram and initializing the model and any templates.
- * The model's data should not be set here, as the ReactDiagram component handles that via the other props.
- */
-function initDiagram() {
-    const $ = go.GraphObject.make;
-    const diagram =
-        $(go.Diagram,
-            {
-                'undoManager.isEnabled': true,  // must be set to allow for model change listening
-                // 'undoManager.maxHistoryLength': 0,  // uncomment disable undo/redo functionality
-                'clickCreatingTool.archetypeNodeData': { text: 'new node', color: 'lightblue' },
-                model: $(go.GraphLinksModel,
-                    {
-                        linkKeyProperty: 'key'  // IMPORTANT! must be defined for merges and data sync when using GraphLinksModel
-                    })
-            });
+    componentDidMount = () => {
+        let nid = 0;
+        this.setState({
+            w: window.innerWidth,
+            h: window.innerHeight
+        });
+        console.log("DFF");
+        this.cy.on('click', (event) => {
+            console.log("dddddddddddddddd");
+            nid = nid +1;
+            this.new_node(nid)
+        });
 
-    // add grid in background
-    diagram.grid.visible = true;
+    };
 
-    // define a simple Node template
-    diagram.nodeTemplate =
-        $(go.Node, 'Auto',  // the Shape will go around the TextBlock
-            new go.Binding('location', 'loc', go.Point.parse).makeTwoWay(go.Point.stringify),
-            $(go.Shape, 'RoundedRectangle',
-                { name: 'SHAPE', fill: 'white', strokeWidth: 0 },
-                // Shape.fill is bound to Node.data.color
-                new go.Binding('fill', 'color')),
-            $(go.TextBlock,
-                { margin: 8, editable: true },  // some room around the text
-                new go.Binding('text').makeTwoWay()
-            )
-        );
+    new_node = (id) => {
+        this.cy.on('click', (event) => {
+            console.log("dddddddddddddddd");
+            this.cy.add({
+                data: { id: id, label: 'Node swdwweew' }, position: { x: 400, y: 400 } }
+            );
+        });
+    };
 
-    return diagram;
-}
+    setUpListeners = () => {
+        console.log("DFF");
+        this.cy.on('click', 'node', (event) => {
+            console.log("dddddddddddddddd")
+        })
+    }
 
-/**
- * This function handles any changes to the GoJS model.
- * It is here that you would make any updates to your React state, which is dicussed below.
- */
-function handleModelChange(changes) {
-}
+    render() {
+        return (
+            <div>
+                <CytoscapeComponent
+                    elements={this.state.elements}
+                    style={{width: this.state.w, height: this.state.h}}
+                    cy={(cy) => {
+                        this.cy = cy
+                    }}
 
-// render function...
-export function GraphEditor() {
-    return (
-        <div>
-            <ReactDiagram
-                initDiagram={initDiagram}
-                divClassName='diagram-component'
-                nodeDataArray={[
-                    { key: 0, text: 'Alpha', color: 'lightblue', loc: '0 0' },
-                    { key: 1, text: 'Beta', color: 'orange', loc: '150 0' },
-                    { key: 2, text: 'Gamma', color: 'lightgreen', loc: '0 150' },
-                    { key: 3, text: 'Delta', color: 'pink', loc: '150 150' }
-                ]}
-                linkDataArray={[
-                    { key: -1, from: 0, to: 1 },
-                    { key: -2, from: 0, to: 2 },
-                    { key: -3, from: 1, to: 1 },
-                    { key: -4, from: 2, to: 3 },
-                    { key: -5, from: 3, to: 0 }
-                ]}
-                onModelChange={handleModelChange}
-             skipsDiagramUpdate={false}/>
-        </div>
-    );
+                />
+            </div>
+        )
+    }
 }
