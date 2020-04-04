57214bfdbff0da9efc3f1be816d4755c0e3472a3
giusdp <depalma.gsp@gmail.com>
2020-04-04 17:14:34 +0200

    Change footer spacing, list of links is now just text and smaller shadow

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,39 +5,36 @@ export class Footer extends React.Component {
 
 	render() {
 		return (
-			<footer style={{boxShadow: "0 -8px 16px rgba(0,0,0,.15)"}}
-			className="page-foot container-fluid text-center text-md-left font-small blue pt-4">
+			<footer
+				// style={{
+				// 	boxShadow: "0 -8px 16px rgba(0,0,0,.15)"
+				// }}
+				className="container-fluid text-center text-md-left font-small info pt-2 shadow-lg
+				rounded border-info">
 
-					<div className="row justify-content-md-around">
+				<div className="row justify-content-md-around">
 
-						<div className="col-md-6 mt-md-0 mt-3">
+					<div className="col-md-6">
 
-							<h5 className="text-uppercase">GISILO</h5>
-							<p>Slogan to be found? Or something else</p>
-
-						</div>
-
-						<div className="col-md-3 mb-md-0 mb-3">
-
-							<h5 className="text-uppercase">CONNECT</h5>
-
-							<ul className="list-unstyled">
-								<li>
-									<a href="#!">Meet the team</a>
-								</li>
-								<li>
-									<a href="#!">Contacts Us</a>
-								</li>
-								<li>
-								TODO: metti riga con icone github e altri social che non abbiamo
-								</li>
-							</ul>
-
-						</div>
+						<h5 className="text-uppercase">Gisilo</h5>
+						<p>Slogan to be found? Or something else</p>
 
 					</div>
 
-				<div className="footer-copyright text-center py-3">© 2020 Copyright: Gisilo</div>
+					<div className="col-md-3 mb-md-0 mb-1">
+
+						<h5 className="text-uppercase">Connect</h5>
+
+						<ul className="list-unstyled">
+							<li href="#!">Meet the team</li>
+							<li href="#!">Contacts Us</li>
+							<li>TODO: riga con icone github e altri social</li>
+						</ul>
+					</div>
+
+				</div>
+
+				<div className="footer-copyright text-center pb-2">© 2020 Copyright: Gisilo</div>
 			</footer>
 		);
 	}
