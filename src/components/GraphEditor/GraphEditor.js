6a95a74eb7c363fd86f353361127cbe1ecebe911
Simone-Damico <simodamico1992@gmail.com>
2020-04-05 11:50:47 +0200

    Add layout

--- src/components/GraphEditor/GraphEditor.js
+++ src/components/GraphEditor/GraphEditor.js
@@ -9,41 +9,45 @@ export class GraphEditor extends Component {
         h: 0,
         elements: [
             { data: { id: 'one', label: 'Node 1wwww' }, position: { x: 200, y: 200 } },
-            { data: { id: 'two', label: 'Node 2' }, position: { x: 100, y: 0 }},
+            { data: { id: 'two', label: 'Node 2' }, position: { x: 100, y: 100 }},
             { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2'}}
-        ]
+        ],
+
     };
+
+    options = {
+        name: 'grid',
+
+        };
 
     componentDidMount = () => {
         let nid = 0;
         this.setState({
             w: window.innerWidth,
-            h: window.innerHeight
+            h: window.innerHeight,
+            layout: {name: 'cose'}
         });
+        //let la = this.cy.layout( this.options );
+        //la.run();
         console.log("DFF");
         this.cy.on('click', (event) => {
             console.log("dddddddddddddddd");
             nid = nid +1;
-            this.new_node(nid)
+            this.new_node(nid, event.position.x, event.position.y);
+            this.cy.layout(this.state.layout).run();
+            this.cy.fit();
+
         });
 
     };
 
-    new_node = (id) => {
-        this.cy.on('click', (event) => {
-            console.log("dddddddddddddddd");
-            this.cy.add({
-                data: { id: id, label: 'Node swdwweew' }, position: { x: 400, y: 400 } }
-            );
-        });
+    new_node = (id, pos_x, pos_y) => {
+        this.cy.add({
+            data: { id: id, label: 'Node ' + id },
+            position: { x: pos_x, y: pos_y } }
+        ).css({'background-color' : 'blue'});
     };
 
-    setUpListeners = () => {
-        console.log("DFF");
-        this.cy.on('click', 'node', (event) => {
-            console.log("dddddddddddddddd")
-        })
-    }
 
     render() {
         return (
@@ -52,7 +56,9 @@ export class GraphEditor extends Component {
                     elements={this.state.elements}
                     style={{width: this.state.w, height: this.state.h}}
                     cy={(cy) => {
-                        this.cy = cy
+                        this.cy = cy;
+
+
                     }}
 
                 />
