a1cbf5bc58f9ac1d4899498a0cb99cc6451975ba
giusdp <depalma.gsp@gmail.com>
2020-04-06 19:24:00 +0200

    Delete unused files and test title with new font

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Navbar, Nav, Button} from 'react-bootstrap/';
+import { Navbar, Nav, Button } from 'react-bootstrap/';
 
 export class AppBar extends React.Component {
 
@@ -8,18 +8,18 @@ export class AppBar extends React.Component {
 
     return (
       <Navbar bg="primary" expand="lg" className="shadow-sm rounded-lg border">
-        <Navbar.Brand href="#home">
-      Gra.sk
-    </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
+          <Nav>
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#project">New Project</Nav.Link>
             <Nav.Link href="#docs">Docs</Nav.Link>
           </Nav>
-          <Button variant="outline-info" style={{"border-radius": "35px"}} size="sm" className="ml-2  ">Log In</Button>{' '}
-          <Button variant="info" style={{"border-radius": "35px"}} className="ml-2">Sign Up</Button>{' '}
+          <Navbar.Brand href="#home" className="mx-auto" style={{"font-family":"Pacifico-Regular"}}>
+            Gra.sk
+          </Navbar.Brand>
+          <Button variant="outline-secondary" style={{ "border-radius": "35px" }} className="ml-2  ">Log In</Button>{' '}
+          <Button variant="info" style={{ "border-radius": "35px" }} className="ml-2">Sign Up</Button>{' '}
         </Navbar.Collapse>
       </Navbar>
     );
