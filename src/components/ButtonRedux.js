bd32677d5b979956ee291e92d80790938fb8e061
giusdp <depalma.gsp@gmail.com>
2020-04-18 12:04:24 +0200

    Install axios and function to send post request

--- src/components/ButtonRedux.js
+++ src/components/ButtonRedux.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import { connect } from 'react-redux';
+import axios from 'axios';
 
 function mapStateToProps(state, ownProps) {
 	return {
@@ -10,9 +11,30 @@ function mapStateToProps(state, ownProps) {
 
 class ButtonRedux extends React.Component {
 
+	saveWork = () => {
+		let responsePromise = axios.post('127.0.0.1:8000/graphql', {
+			query: {
+				allGrabits: {
+					edges: {
+						node:
+							[
+								"id",
+								"nameProject",
+								"nameDb"
+							]
+					}
+				}
+			}
+		});
+		responsePromise.then(
+			(response) => { console.log(response); },
+			(error) => { console.log(error); }
+		);
+	}
+
 	render() {
 		return (
-			<Button onClick={() => console.log("Sono simonfox")} className="px-5" variant={this.props.variant}>
+			<Button onClick={() => this.saveWork()} className="px-5" variant={this.props.variant}>
 				{this.props.text}
 			</Button>
 		);
