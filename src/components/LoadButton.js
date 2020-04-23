c6e530f155f4bf9fa1f4529c409160c26e3e7b70
Simone-Damico <simodamico1992@gmail.com>
2020-04-23 16:57:17 +0200

    Component for load an existing Grabit project

---
+++ src/components/LoadButton.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+import gql from 'graphql-tag'
+import { withApollo } from 'react-apollo';
+
+
+class LoadButton extends React.Component {
+
+    loadProject = () => {
+        this.props.client.query({
+            query: gql
+                `query GetGrabitByName($projectName: String!){
+                    allGrabits(nameProject: $projectName){
+                        edges{
+                            node{
+                                id
+                                nameProject
+                                nameDb
+                                dbms
+                                description
+                                port
+                                createdDate
+                                updateDate
+                                graph
+                            }
+                        }
+                    }
+				}`,
+            variables: {
+                projectName: "dddd"
+            }
+        }).then(
+            (success) => {
+                console.log("suc", success);
+                const currentProject = success.data.allGrabits.edges[0].node;
+                currentProject.graph = JSON.parse(currentProject.graph);
+                console.log(currentProject);
+
+            },
+
+            (error) => console.log("err", error))
+
+    };
+
+    render() {
+        return (
+            <Button onClick={this.loadProject} className="px-5" variant={this.props.variant}>
+                {this.props.text}
+            </Button>
+
+        );
+    }
+
+}
+
+export default withApollo(LoadButton);
