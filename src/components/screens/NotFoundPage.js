eda607afdbc7e1e383509dc3c7decb99ccaad13f
giusdp <depalma.gsp@gmail.com>
2020-05-10 10:59:33 +0200

    Install react router bootstrap and react router dom, add 404 page and create landing page file

---
+++ src/components/screens/NotFoundPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+export class EditorScreen extends React.Component{
+	render() {
+		return (
+			<Container fluid>
+				<h1>404</h1><h2>Oh shit bois</h2>
+			</Container>
+
+		)
+	}
+}
