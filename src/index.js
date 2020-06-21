8d19c1846d88bd0874d9c0f672b31c12c909fb10
giusdp <depalma.gsp@gmail.com>
2020-06-21 11:51:28 +0200

    Remove style folder and move indes.css in root folder. Uninstall node-sass

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './logged_in/styles/index.css';
+import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
