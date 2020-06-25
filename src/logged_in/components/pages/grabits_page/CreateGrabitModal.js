949c09b86325e91868a299121f067d08cda70442
giusdp <depalma.gsp@gmail.com>
2020-06-25 15:23:57 +0200

    Integrate Formik in create grabit modal and mutation on submit

--- src/logged_in/components/pages/grabits_page/CreateGrabitModal.js
+++ src/logged_in/components/pages/grabits_page/CreateGrabitModal.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import Slide from "@material-ui/core/Slide";
 import Zoom from "@material-ui/core/Zoom";
+import {Form, Formik} from "formik";
+import FormikTextField from "../../../../common/components/FormikTextField";
+import * as yup from "yup";
+import {useMutation} from "@apollo/react-hooks";
+import {CREATE_QUERY} from "../../../../common/costants/queries";
 
 // This generates findDomNode warning, TODO: find alternative to forwardRef
 const Transition = React.forwardRef(function Transition(props, ref) {
@@ -16,6 +19,7 @@ const Transition = React.forwardRef(function Transition(props, ref) {
 
 export default function CreateGrabitModal(props) {
     const {open, handleClose} = props;
+    const [addGrabit] = useMutation(CREATE_QUERY);
 
     return (
         <div>
@@ -24,29 +28,63 @@ export default function CreateGrabitModal(props) {
                 onClose={handleClose}
                 aria-labelledby="form-dialog-title"
                 TransitionComponent={Transition}>
-                <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
-                <DialogContent>
-                    <DialogContentText>
-                        To subscribe to this website, please enter your email address here. We will send updates
-                        occasionally.
-                    </DialogContentText>
-                    <TextField
-                        autoFocus
-                        margin="dense"
-                        id="name"
-                        label="Email Address"
-                        type="email"
-                        fullWidth
-                    />
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose} color="primary">
-                        Cancel
-                    </Button>
-                    <Button onClick={handleClose} color="primary">
-                        Subscribe
-                    </Button>
-                </DialogActions>
+                <DialogTitle id="form-dialog-title">Create new Grabit</DialogTitle>
+                <Formik
+                    initialValues={{grabitName: "", description: ""}}
+                    // validationSchema={yup.object({grabitName: yup.string().required("Grabit name required")})}
+                    onSubmit={(data, { setSubmitting }) => {
+                        setSubmitting(true);
+                        // query to create grabit
+                        addGrabit({
+                            variables: {
+                                nameGrabit: data.grabitName,
+                                descr: data.description
+                            }
+                        }).then((success) => {
+                            console.log('success', success);
+                        }, (error) => console.error('error', error));
+                        handleClose();
+                        setSubmitting(false);
+                    }}>
+                    {(isSubmitting) =>
+                        (<Form>
+                        <DialogContent>
+                            <DialogContentText>
+                                Provide a name and an (optional) description to start working.
+                            </DialogContentText>
+                            <FormikTextField
+                                autoFocus
+                                margin="dense"
+                                id="name"
+                                label="Grabit Name"
+                                type="text"
+                                name="grabitName"
+                                fullWidth
+                                required
+                            />
+                            <FormikTextField
+                                margin="dense"
+                                id="description"
+                                label="Description"
+                                type="text"
+                                name="description"
+                                fullWidth
+                                multiline
+                                rows={2}
+                                maxRows={4}
+                            />
+                        </DialogContent>
+                        <DialogActions>
+                            <Button onClick={handleClose} color="primary">
+                                Cancel
+                            </Button>
+                            <Button disabled={!isSubmitting} type="submit" color="primary">
+                                Create Grabit
+                            </Button>
+                        </DialogActions>
+                        </Form>)
+                    }
+                </Formik>
             </Dialog>
         </div>
     );
