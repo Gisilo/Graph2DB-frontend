91e8faba613cfe55b1a97335636f53a6b2d0cc00
Simone-Damico <simodamico1992@gmail.com>
2020-07-27 12:00:56 +0200

    Move fab in graphEditor and save grabit names in recoil state to create new untitle grabit

--- src/logged_in/components/pages/editor_page/EditorPage.js
+++ src/logged_in/components/pages/editor_page/EditorPage.js
@@ -2,9 +2,7 @@ import React, {useState} from 'react';
 import GraphEditor from '../../GraphEditor/GraphEditor';
 import Grid from "@material-ui/core/Grid";
 import NavBar from "../../navbar/NavBar";
-import Fab from "@material-ui/core/Fab";
 import {makeStyles} from "@material-ui/core/styles";
-import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 import {authenticationService} from "../../../../common/services/authenticationService";
 import {GET_GRABITS_BY_ID_AND_OWNER, CREATE_MUT} from "../../../../common/costants/queries";
 import {withApollo} from "@apollo/react-hoc";
@@ -55,16 +53,6 @@ const EditorPage = (props) => {
                     <GraphEditor heightOffset={heightOffset} graph={graph} idGrabit={idGrabit}/>
                 </Grid>
             </Grid>
-            <Fab
-                variant="extended"
-                size="medium"
-                color="primary"
-                aria-label="add node"
-                className={classes.fab}
-            >
-                <AddCircleOutlineIcon className={classes.extendedIcon}/>
-                Add Node
-            </Fab>
         </>
     );
 };
